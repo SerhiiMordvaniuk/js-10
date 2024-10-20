e80db94032689e53527108f85469e1bed04b7544
Serhii Mordvaniuk <sm636332@gmail.com>
2024-10-20 10:02:47 +0200

    aaaaa

--- 1.js
+++ 1.js
@@ -24,18 +24,63 @@
 //     }, 1000)
 // }
 
-const date = new Date();
-console.log(typeof date);
+// const date = new Date();
+// console.log(typeof date);
  
-const startTime = Date.now();
+// const startTime = Date.now();
 
-// Твій код, виконуваний упродовж деякого часу
-for(let i = 0; i <= 100 ; i += 1) {
-	console.log(i);
-}
+// // Твій код, виконуваний упродовж деякого часу
+// for(let i = 0; i <= 100 ; i += 1) {
+// 	console.log(i);
+// }
 
-const endTime = Date.now();
-const elapsedTime = endTime - startTime;
+// const endTime = Date.now();
+// const elapsedTime = endTime - startTime;
 
-console.log(`Elapsed time: ${elapsedTime} ms`);
+// console.log(`Elapsed time: ${elapsedTime} ms`);
+
+// const btn = document.querySelector(".btn");
+// const body = document.querySelector("body")
+
+// console.log(body);
+// const timer = setTimeout(() => {
+// 		body.style.background = "green"
+// 	}, 5000)
+// const foo = () => {
+// 	console.log("ok");
+
+// }
+
+
+// clearTimeout(timer)
+
+// const btn = document.querySelector('.btn');
+
+// function foo() {
+// 	console.log("ok");
+	
+// }
+// const interval = setInterval(() => {
+// 	console.log("alalla");
+// 	foo()
+// }, 2000)
+
+// clearInterval(interval)
+
+// const start = Date.now();
+// for (let i = 0; i < 1; i++) {
+// 	console.log(i);
+	
+// }
+
+// let finish = Date.now();
+// const lalala = finish - start
+
+// console.log(`${lalala} ms`);
+
+
+ 
+// const now = Date.now();
+// console.log(now);
+// console.log(getDay());
 
