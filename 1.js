3b277f6c96617a5085915383ac064240c4dc76aa
Serhii Mordvaniuk <sm636332@gmail.com>
2024-10-21 18:13:09 +0200

    a

--- 1.js
+++ 1.js
@@ -134,3 +134,28 @@
 // }
 
 // getdate(date)
+
+// const isSuccess = true;
+
+// // Create promise
+// const promise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     if (isSuccess) {
+//       resolve("Success! Value passed to resolve function");
+//     } else {
+//       reject("Error! Error passed to reject function");
+//     }
+//   }, 2000);
+// });
+
+
+// promise.then(
+//   value => {
+//     console.log(value); // "Success! Value passed to resolve function"
+//   },
+//   error => {
+//     console.log(error); // "Error! Error passed to reject function"
+//   }
+// );
+
+ 
